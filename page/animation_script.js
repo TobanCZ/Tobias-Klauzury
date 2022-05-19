e571db157b1b4cd6d68933b5360e567d44ec1d52
TobanCZ <tobiasgameing6@gmail.com>
2022-05-20 01:54:47 +0200

    asd

--- page/animation_script.js
+++ page/animation_script.js
@@ -1,8 +1,29 @@
+var isPressd = false;
+var timer ;
+
 document.getElementById("menu").addEventListener("click", function(){ 
-	this.classList.toggle("toggle") 										//přepínání tříd u hamurgr menu
+	if(isPressd == false)
+	{
+		this.classList.toggle("toggle") 			//přepínání tříd u hamurgr menu
+
+		if(document.getElementById("navigation").className == "")		//přepínání navigace
+		{
+			document.getElementById("navigation").classList.toggle("show")
+		}		
+		else
+		{
+			document.getElementById("navigation").classList.toggle("show")
+			document.getElementById("navigation").classList.toggle("hide")
+		}	
+
+		isPressd = true;
+		timer = setInterval(Timer, 500);
+	}
 }) 
 
-document.getElementById("dark").addEventListener("click", function(){
-	document.getElementById("icon_dark").classList.toggle("toggle")
-	document.getElementById("icon_light").classList.toggle("toggle")
-});
+function Timer()
+{
+	clearInterval(timer);
+	isPressd = false;
+}
+

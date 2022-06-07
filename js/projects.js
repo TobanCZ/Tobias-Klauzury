2510f76a32054a9c5530462f7d9d6c1ac6d8927f
TobanCZ <tobiasgameing6@gmail.com>
2022-06-08 01:30:43 +0200

    main

--- js/projects.js
+++ js/projects.js
@@ -14,6 +14,37 @@ function body_scroll()
     
 }
 
+//* ------------------ mobile nav menu button ------------------
+
+var isPressd = false;
+var timer ;
+
+document.getElementById("menu").addEventListener("click", function(){ 
+	if(isPressd == false)
+	{
+		this.classList.toggle("toggle") 			//přepínání tříd u hamurgr menu
+
+		if(document.getElementById("navigation").className == "")		//přepínání navigace
+		{
+			document.getElementById("navigation").classList.toggle("show")
+		}		
+		else
+		{
+			document.getElementById("navigation").classList.toggle("show")
+			document.getElementById("navigation").classList.toggle("hide")
+		}	
+
+		isPressd = true;
+		timer = setInterval(Timer, 500);
+	}
+}) 
+
+function Timer()
+{
+	clearInterval(timer);
+	isPressd = false;
+}
+
 //* ------------------ favourite show and hide info ------------------
 
 function show_info(info)
